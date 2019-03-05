edde3fe4948993ac84661f1c1c1cecc164e6732e
hemant <nhemnt@gmail.com>
2019-03-05 22:50:51 +0530

    webpack and babel configure

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+  
+  render() {
+    return(
+      <div>
+        Project Structure
+      </div>
+  );
+}
+}
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      },
+      // Scss compiler
+      {
+          test: /\.scss$/,
+          use: [
+              MiniCssExtractPlugin.loader,
+              "css-loader",
+              "sass-loader"
+          ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "assets/css/[name].[hash:8].css"
+    })
+  ]
+};
